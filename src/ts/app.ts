c70786cb3e78b479718cc1f18afc43ba53b8220c
funnythingz <funnythingz69+github@gmail.com>
2015-03-18 00:30:22 +0900

    wip XHR

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,1 +1,35 @@
 /// <reference path="../../typings/tsd.d.ts" />
+
+$(() => {
+    var promise = $.ajax({
+        type: 'get',
+        url: 'http://localhost:3000/api/v1/entries',
+        dataType: 'json',
+        async: true
+    });
+
+    promise.done(data => {
+        console.log('done');
+        console.log(data);
+    });
+
+    $('#post-action').on('click', e => {
+        var promise = $.ajax({
+            type: 'post',
+            url: 'http://localhost:3000/api/v1/entries',
+            dataType: 'json',
+            data: {
+                entries: {
+                    title: 'hoge',
+                    content: 'hogehoge'
+                }
+            },
+            async: true
+        });
+
+        promise.done(data => {
+            console.log('done');
+            console.log(data);
+        });
+    });
+});
